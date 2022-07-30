0785b12e37328bbbfdb5de83130fec1cbb49631f
patrickfenn <patrickdavidfenn@gmail.com>
2022-07-29 19:35:35 -0700

    switched to webserver instead of localhost

--- src/main/java/group1/cs180_proj/Http.java
+++ src/main/java/group1/cs180_proj/Http.java
@@ -37,7 +37,7 @@ public class Http {
    
    public  ArrayList<Uber> getSearch(String column, String entry) throws Exception {
        lastSearch = new Search(column, entry);
-       String ip = "http://localhost:3000/search/";
+       String ip = "http://134.122.39.21:3000/search/";
        ArrayList<Uber> ret = new ArrayList();
        URL url = new URL(ip);
        HttpURLConnection con = (HttpURLConnection) url.openConnection();
@@ -79,7 +79,7 @@ public class Http {
     }
      public  String getAnalysis() throws Exception {
        
-       String ip = "http://localhost:3000/analysis/";
+       String ip = "http://134.122.39.21:3000/analysis/";
        String ret;
        URL url = new URL(ip);
        HttpURLConnection con = (HttpURLConnection) url.openConnection();
@@ -109,7 +109,7 @@ public class Http {
     }
  
      public  Uber postInsert(Uber u) throws Exception {
-       String ip = "http://localhost:3000/insert/";
+       String ip = "http://134.122.39.21:3000/insert/";
 
        URL url = new URL(ip);
        HttpURLConnection con = (HttpURLConnection) url.openConnection();
@@ -142,7 +142,7 @@ public class Http {
         
     }
      public  void postEdit(Uber u) throws Exception {
-       String ip = "http://localhost:3000/edit/";
+       String ip = "http://134.122.39.21:3000/edit/";
       
        URL url = new URL(ip);
        HttpURLConnection con = (HttpURLConnection) url.openConnection();
@@ -176,7 +176,7 @@ public class Http {
     }
      
       public  void postRemove(String i) throws Exception {
-       String ip = "http://localhost:3000/remove/";
+       String ip = "http://134.122.39.21:3000/remove/";
      
        URL url = new URL(ip);
        HttpURLConnection con = (HttpURLConnection) url.openConnection();
